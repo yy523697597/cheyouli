4f480d3b1657872726b81c625d238eb28dbfac77
yu yi <523697597@qq.com>
2016-11-05 23:29:08 +0800

    V1.0.3 优化JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,44 @@
 /**
  * Created by yu on 2016/11/4 0004.
  */
+//首页轮播图
 $(function () {
-	$('.banner').unslider({
-		animation: 'vertical',
-		speed: 500,               //  The speed to animate each slide (in milliseconds)
-		delay: 5000,              //  The delay between slide animations (in milliseconds)
-		complete: function () {
-		},  //  A function that gets called after every slide animation
-		keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-		dots: true,               //  Display dot navigation
-		autoplay: true,
-		fluid: false              //  Support responsive design. May break non-responsive designs
-	});
+  $('.banner').unslider({
+    animation: 'vertical',    //  切换动画为垂直
+    speed: 500,               //  切换动画时间
+    delay: 5000,              //  切换动画间隔
+    keys: true,               //  允许使用键盘操作
+    dots: true,               //  使用导航点
+    autoplay: true,           //  自动播放
+    fluid: false              //  不支持响应式
+  });
 });
-$('#locationSelect').citys({ province: '四川', city: '成都', area: '新都' });
 
-//  comment列表滚动
+//  选择区域省市级三级联动，默认为新都
+$('#locationSelect').citys({province: '四川', city: '成都', area: '新都'});
+
+//  comment列表垂直滚动
 $(function () {
-	var $comment = $("#comment");
-	var scrollTimer;
-	$comment.hover(function () {
-		clearInterval(scrollTimer);
-	}, function () {
-		scrollTimer = setInterval(function () {
-			scrollNews($comment);
-		}, 3000);
-	}).trigger("mouseleave");
+  var $comment = $('#comment');
+  var scrollTimer;
+  $comment.hover(function () {
+    clearInterval(scrollTimer);
+  }, function () {
+    scrollTimer = setInterval(function () {
+      scrollNews($comment);
+    }, 3000);
+  }).trigger('mouseleave');
 
-	function scrollNews(obj) {
-		var $self = obj.find("ul");
-		var lineHeight = $self.find("li:first").height();
-		$self.animate({
-			"marginTop": -lineHeight + "px"
-		}, 700, function () {
-			$self.css({
-				marginTop: 0
-			}).find("li:first").appendTo($self);
-		})
-	}
-})
+  function scrollNews(obj) {
+    var $self = obj.find('ul');
+    var lineHeight = $self.find('li:first').height();
+    $self.animate({
+      'marginTop': -lineHeight + 'px'
+    }, 700, function () {
+      $self.css({
+        marginTop: 0
+      }).find('li:first').appendTo($self);
+    });
+  }
+});
+
